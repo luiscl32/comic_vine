c10925ee3d37f895fc95e23bc0c735e7a3c3ec2b
Luis Correa <luiscl32@gmail.com>
2023-07-23 01:21:46 -0400

    feat: :art: adding bloc provider and builder in comic list

--- lib/presentation/screens/comic_list/comic_list.page.dart
+++ lib/presentation/screens/comic_list/comic_list.page.dart
@@ -1,5 +1,7 @@
+import 'package:comic_vine/domain/blocs/cubit/comic_list_cubit.dart';
 import 'package:comic_vine/presentation/screens/comic_list/comic_list.view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ComicListPage extends StatelessWidget {
   const ComicListPage({Key? key}) : super(key: key);
@@ -16,11 +18,14 @@ class ComicListPage extends StatelessWidget {
           fontSize: 28,
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
           horizontal: 16,
         ),
-        child: ComicListView(),
+        child: BlocProvider(
+          create: (_) => ComicListCubit()..getLatestIssues(),
+          child: const ComicListView(),
+        ),
       ),
     );
   }
--- lib/presentation/screens/comic_list/comic_list.view.dart
+++ lib/presentation/screens/comic_list/comic_list.view.dart
@@ -1,8 +1,63 @@
+import 'package:comic_vine/domain/blocs/cubit/comic_list_cubit.dart';
 import 'package:comic_vine/presentation/screens/comic_list/widgets/comic_list.widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ComicListView extends StatelessWidget {
   const ComicListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ComicListCubit, ComicListState>(
+      builder: (context, state) {
+        return state.when(
+          initial: () {
+            return Container();
+          },
+          loading: () {
+            return const LoadingView();
+          },
+          loaded: (lastIssuesData) {
+            print(lastIssuesData.results![0].name);
+            return const LoadedView();
+          },
+          error: () {
+            return const ErrorView();
+          },
+        );
+      },
+    );
+  }
+}
+
+class ErrorView extends StatelessWidget {
+  const ErrorView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Error'));
+  }
+}
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('loading ...'),
+    );
+  }
+}
+
+class LoadedView extends StatelessWidget {
+  const LoadedView({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
